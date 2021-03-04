c726db6d1d15fa545a59dc23b4eac2aa4469b06c
SurajSSingh <surajss@uci.edu>
2021-03-03 18:34:28 -0800

    Player Behavior Tree

---
+++ Assets/Scripts/PlayerNavAgentStoppedCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pada1.BBCore.Framework;
+using Pada1.BBCore;
+
+[Condition("MyConditions/Player/CheckNavMeshAgentStopped")]
+public class PlayerNavAgentStoppedCheck : ConditionBase
+{
+    [InParam("PlayerNavAgent")]
+    public UnityEngine.AI.NavMeshAgent navAgent;
+
+    private bool alreadyChecked;
+
+    public override bool Check()
+    {
+        if (navAgent == null)
+        {
+            return false;
+        }
+        if (alreadyChecked)
+        {
+
+        }
+        else
+        {
+
+        }
+        return navAgent.isStopped;
+    }
+}
